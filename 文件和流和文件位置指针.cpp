238fc1642de6c7dc8f9241b1b52a4d22c6f1ad70
jisaber <1721390692@qq.com>
2017-11-01 17:13:34 +0800

    Update and rename 文件和流.cpp to 文件和流和文件位置指针.cpp

--- 文件和流和文件位置指针.cpp
+++ 文件和流和文件位置指针.cpp
@@ -64,3 +64,19 @@ zhuxiang
 22
 请按任意键继续. . .
 */
+/*
+ifstream 有seekg（“seek get”）  
+ofstream 有seekp（“seek put”）
+// 定位到 fileObject 的第 n 个字节（假设是 ios::beg）
+fileObject.seekg( n );
+ 
+// 把文件的读指针从 fileObject 当前位置向后移 n 个字节
+fileObject.seekg( n, ios::cur );
+ 
+// 把文件的读指针从 fileObject 末尾往回移 n 个字节
+fileObject.seekg( n, ios::end );
+ 
+// 定位到 fileObject 的末尾
+fileObject.seekg( 0, ios::end );
+
+*/
