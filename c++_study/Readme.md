a09d0053a628eb6ed2bcbc42b4828f83bde53872
zhuxiang <zhuxxxiang@qq.com>
2024-05-18 16:38:57 +0800

    fix:完善的编译脚本 DIR:2024-05-18

---
+++ c++_study/Readme.md
@@ -0,0 +1,3 @@
+到build目录执行
+sh build.sh -f <filename>.cpp -o <outfilename>
+会自动编译执行，二进制文件在output/aarch64目录
---
+++ c++_study/build/build.sh
@@ -0,0 +1,32 @@
+
+date
+outdir="./output/aarch64"
+filename=""
+outname=""
+
+usage()
+{
+    echo "help"
+}
+
+gccmake()
+{
+    outfile=$outdir/$outname
+    echo "source: $filename "
+    echo "result: $outfile"
+    g++ -o $outfile $filename
+    echo "=================== below is bin result============"
+    ./$outfile
+}
+
+while getopts "f:o:h" opt;do
+    case $opt in
+        f)
+            filename=$OPTARG;;
+        o)
+            outname=$OPTARG;;
+        h) usage;;
+    esac
+done
+
+gccmake
